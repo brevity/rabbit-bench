22281b3b6e8e7b01f4bdce1a74f0af16dd6e4fbd
Andrew Toelle <andrew.toelle@gmail.com>
2014-03-27 11:20:25 -0500

    clean things up a bit

---
+++ README.md
@@ -0,0 +1,19 @@
+You might need to install some Stuff.
+
+- install [rabbitMQ] (https://www.rabbitmq.com/install-homebrew.html)
+- [nodejs](http://nodejs.org/)
+
+And you will definitely need to do some stuff
+- install node requirements
+```bash
+$ npm install
+```
+
+- open up the .env file and put in your production amqp uri
+- source that
+
+```bash
+$ source .env
+```
+
+
--- ping.js
+++ ping.js
@@ -3,33 +3,38 @@ var argv        = require('minimist')(process.argv.slice(2)),
     colors      = require('colors'),
     rabbitDev   = 'amqp://localhost',
     rabbitPro   = process.env.AMQP_SRVR,
-    queue       = 'new-articles',
     cliMessage  = process.argv.splice(2).join(" ") || "n/a",
     encoding    = 'utf8';
 
 var rabbitServer = argv.p ? rabbitPro : rabbitDev;
-var socketType   = argv.socketType || 'PUSH';
+var socketType   = argv.type || 'PUSH';
+var queue      = argv.q || 'test';
 
-// Prep some test objects to pass down to pipe.
-var doi = {};
-doi.message = 'sent from pinger';
-doi.doi = '10.4161/biom.22905';
-doi = JSON.stringify(doi);
+// HEY! Prep some test objects to pass down to pipe.
+var article = {};
+article.doi = '10.4161/biom.22905';
+article = JSON.stringify(article);
 
+// Show the user a friendly suggestion.
+console.log("----- Time To Start Pinging! -------".green);
 
 //Connect to RabbitMQ server and prompt user for input
-var context       = require('rabbit.js').createContext(rabbitServer);
-  context.on('error', function(e){
-    console.log("[ rabbitMQ is down :-( ]".red);
-    console.log(e.grey);
-  });
+var context = require('rabbit.js').createContext(rabbitServer);
+
+context.on('error', function(e){
+  console.log("[ rabbitMQ is down :-( ]".red);
+  console.log(e.grey);
+});
+
 context.on('ready', function(){
   //connect to server
-  console.log("[connecting] ".green + rabbitServer.blue);
+  console.log("[connecting to server] ".green + rabbitServer.blue);
   var pub = context.socket(socketType);
   // socket created
   pub.setsockopt('persistent', true);
   pub.connect(queue, function(){
+  console.log("[socketType] ".green + socketType.blue);
+  console.log("[queue] ".green + queue.blue);
     process.stdin.resume();
     process.stdin.setEncoding('utf8');
     process.stdin.on('data', function(chunk){
@@ -40,8 +45,8 @@ context.on('ready', function(){
           process.exit();
           break;
         case '':
-        case 'doi':
-          pub.write(doi, 'utf8');
+        case 'article':
+          pub.write(article, 'utf8');
           break;
         case 'hose':
           console.log("[sending 30 messages to rabbit]".yellow);
